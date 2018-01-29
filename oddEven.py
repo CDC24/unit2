e5af988f3ab8bd6be19d962f08b4a43b147416d0
CDC24 <35492804+CDC24@users.noreply.github.com>
2018-01-29 08:44:16 -0500

    Updated from Brython Server: 1/29/2018 8:44:15 AM

--- oddEven.py
+++ oddEven.py
@@ -1,3 +1,13 @@
 #Caleb Callaway
 #1/29/18
 #oddEven.py - uses if statements to determine if a number is even or odd
+
+
+num= int(input("Enter any integer. "))
+
+if num%2 == 1:
+    print (num,"is odd.")
+elif num%2 == 0:
+    print (num,"is even.")
+elif num == 0:
+    print (num,"is even.")
