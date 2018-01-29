35dc2bba8907f1aeb2c136c0bd095f4a782663a6
CDC24 <35492804+CDC24@users.noreply.github.com>
2018-01-29 08:48:36 -0500

    Updated from Brython Server: 1/29/2018 8:48:35 AM

--- unitConverter.py
+++ unitConverter.py
@@ -2,4 +2,8 @@
 #1/29/18
 #unitConverter.py - uses if statements to allow user to choose a conversion type and perform it
 
-print ("Which conversion would you like to perform?")
+print ("Which conversion would you like to perform?
+1)  Kilometers to meters
+2)  Kilograms to pounds
+3)  Liters to gallons
+4)  Celsius to Fahrenheit")
