8a25a8f23f57a74637151821d347c07228ccd1a0
CDC24 <35492804+CDC24@users.noreply.github.com>
2018-01-29 08:28:01 -0500

    Updated from Brython Server: 1/29/2018 8:28:00 AM

--- gradeCalculator.py
+++ gradeCalculator.py
@@ -3,5 +3,15 @@
 #gradeCalculator.py - uses if statements to change a grade percentage to a letter grade
 
 
-grade = int(input("What is your grade?"))
+grade = int(input("What is your grade? "))
 
+if grade >= 90:
+    print ("You got an A.")
+elif grade >= 80:
+    print ("You got a B.")
+elif grade >= 70:
+    print ("You got a C.")
+elif grade >= 60:
+    print ("You got a D.")
+elif grade < 60:
+    print ("You got an F :(")
