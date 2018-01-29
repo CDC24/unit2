12dcb05f573bfabd5093c235de93700859db0149
CDC24 <35492804+CDC24@users.noreply.github.com>
2018-01-29 08:38:19 -0500

    Updated from Brython Server: 1/29/2018 8:38:18 AM

--- movie.py
+++ movie.py
@@ -1,3 +1,11 @@
 #Caleb Callaway
 #1/29/18
-# movie.py - uses if statements to tell if user of specific age can watch certain movie ratings
+#movie.py - uses if statements to tell if user of specific age can watch certain movie ratings
+
+age = int(input("what is your age? "))
+
+if age >= 18:
+    print ("You could watch up to NC-17 movies.")
+elif age >=13:
+    print ("You could watch up to PG-13 movies, or R movies if accompanied by an adult guardian or parent. ")
+else:print ("You could watch up to PG-13 movies if reviewed by an adult guardian or parent. ")
