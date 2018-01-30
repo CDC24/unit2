a8b8fb579fb17dc0965f09fd1d3accb2af6094f6
CDC24 <35492804+CDC24@users.noreply.github.com>
2018-01-30 08:42:14 -0500

    Updated from Brython Server: 1/30/2018 8:42:13 AM

--- fortuneTeller.py
+++ fortuneTeller.py
@@ -5,7 +5,44 @@
 print ("I shall now commune with the spirits to determine your fortune. ")
 num = int(input("Now... pick a number... one thru four... "))
 color = input ("MmMMmm.... interesting.... now: you must choose: red or blue? ")
- print ("Ah, very nice... let's see.... ")
+print ("Ah, very nice... let's see.... ")
  
- if num==1 and color == "blue":
-     print ("Yess... you will be spooked by a newt on the next full moon.")
+if num==1 and color == "blue":
+    print ("Yess... you will be spooked by a newt on the next full moon.")
+    
+elif num==1 and color == "red":
+    print ("Ohh... a purple peacock will peck and pester your pet pelican.")
+    
+elif num==2 and color == "blue":
+    print ("Mm... make sure your pickles are kosher... they may not be what they seem.")
+    
+elif num==2 and color == "red":
+    print ("Hmm... yes... it's becoming quite clear... you will get a zit on the inside of your nose and it'll be super weird.")
+    
+elif num==3 and color == "blue":
+    print ("Oh!... The winds of fate blow not upon you! It is because of the tinfoil helm upon your head.")
+    
+elif num==3 and color == "red":
+    print ("Ach... an erstwhile yak will fill you with regrets.")
+    
+elif num==4 and color == "blue":
+    print ("Aye... before the year is out, a dog will follow you home!")
+        what = ("wait! Hurry! Choose: Curry or sandwiches??")
+        if what = "curry":
+            print ("Yes! the dog will be friendly!")
+        elif what = "sandwiches":
+            print ("No...! the dog will actually be a demon from the depths of Hell!")
+        else:
+            print ("Mm... the spirits are confused... there may be no dog after all.")
+elif num==4 and color == "red":
+    print ("Yarr!... Scurvy'll take hold'a thee 'n ye'll ne'er see land again!")
+else:
+    print ("Ssss... the spirits are angry with you, child... could it be you entered something wrong?")
+    
+    
+    
+    
+    
+    
+    
+    
