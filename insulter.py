8d5a0a0e0ffe93dc7c0f13537c4140e104a44ea3
CDC24 <35492804+CDC24@users.noreply.github.com>
2018-01-30 16:29:43 -0500

    Updated from Brython Server: 1/30/2018 4:29:43 PM

--- insulter.py
+++ insulter.py
@@ -6,3 +6,20 @@
 from random import randint
 
 num = randint(1,5)
+
+name = input("What's your name?")
+
+if num == 1:
+    print ("Seriously? Your face could scare a vulture away from a pile of animal carcasses.")
+    
+elif num == 2:
+    print ("Who taught you English? An octopus?")
+    
+elif num == 3:
+    print ("Aw, well aren't you special and unique. People must like you a lot.")
+    
+elif num == 4:
+    print ("what's your e-mail,",name,",",name,"@biggiantloser.com?")
+    
+elif num == 5:
+    print ("Jeez, how did your mother feed you without getting close enough to smell you?")
