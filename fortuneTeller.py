5ae739e536937e735d2e8f40c98446f1e2d495c0
CDC24 <35492804+CDC24@users.noreply.github.com>
2018-01-30 08:27:28 -0500

    Updated from Brython Server: 1/30/2018 8:27:27 AM

--- fortuneTeller.py
+++ fortuneTeller.py
@@ -2,4 +2,10 @@
 #1/30/18
 #fortuneTeller.py - distributes fortunes based on the occurence of two independent events.
 
-
+print ("I shall now commune with the spirits to determine your fortune. ")
+num = int(input("Now... pick a number... one thru four... "))
+color = input ("MmMMmm.... interesting.... now: you must choose: red or blue? ")
+ print ("Ah, very nice... let's see.... ")
+ 
+ if num==1 and color == "blue":
+     print ("Yess... you will be spooked by a newt on the next full moon.")
