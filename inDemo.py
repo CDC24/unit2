816aab9421cf3593a6c1b37d3ebb78fb9ebdb8c3
CDC24 <35492804+CDC24@users.noreply.github.com>
2018-01-31 08:07:20 -0500

    Updated from Brython Server: 1/31/2018 8:07:19 AM

--- inDemo.py
+++ inDemo.py
@@ -1,3 +1,11 @@
 #Caleb Callaway
 #1/31/18
-#inDemo.py - conditional statements with strings
+#inDemo.py - conditional statements (the in operator) with strings
+
+
+word = input ("Enter a word. ")
+
+if "w" in word or "W" in word:
+    print (word,"is rad!!")
+else:
+    print (word,"is the worst!")
