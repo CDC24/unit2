04b59e3146c1f81f71d2a295fdb61ba14d082026
CDC24 <35492804+CDC24@users.noreply.github.com>
2018-01-31 08:39:24 -0500

    Updated from Brython Server: 1/31/2018 8:39:23 AM

--- adventure.py
+++ adventure.py
@@ -8,10 +8,15 @@ year = input("what year is it?")
 
 print ("Well,",name,", unfortunately you are lost in a Costco.")
 
-first = input("Do you stay where you are and crack open a crate of Coke?")
+first = input("Do you stay where you are and crack open a crate of Coke? ")
 
 if first == "yes":
     print ("Good choice!")
-    second = ("You see an employee. Do you wave them down and ask for help?")
-    if second == yes:
-        print ("Oooooh... bad move. Never ask for help. YOU LOSE. RIP",name,", died",year.")
+    second = input("You see an employee. Do you wave them down and ask for help? ")
+    if second == "yes":
+        print ("Oooooh... bad move. Never ask for help. YOU LOSE. RIP",name,", died",year)
+    if second == "no":
+        print ("Well done! Assistance is for the weak.")
+        third = input ("You start to get tired. Do you try and stay awake by drinking more COke?")
+        if third == "yes":
+            fourth = input("All that coke is making you feel sick. You throw up. Gross! Do you try to clean it up?")
