76a77e851ff78286907e8cdd81e71653701fb684
CDC24 <35492804+CDC24@users.noreply.github.com>
2018-01-31 08:32:16 -0500

    Updated from Brython Server: 1/31/2018 8:32:15 AM

--- adventure.py
+++ adventure.py
@@ -1,3 +1,17 @@
 #Caleb Callaway
 #1/31/18
 #adventure.py - uses if statements to mimic the computer game adventure
+
+
+name = input("Choose your name. ")
+year = input("what year is it?")
+
+print ("Well,",name,", unfortunately you are lost in a Costco.")
+
+first = input("Do you stay where you are and crack open a crate of Coke?")
+
+if first == "yes":
+    print ("Good choice!")
+    second = ("You see an employee. Do you wave them down and ask for help?")
+    if second == yes:
+        print ("Oooooh... bad move. Never ask for help. YOU LOSE. RIP",name,", died",year.")
