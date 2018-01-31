c9507c63c4e0267bcbd476964a75eae8b3c1b2d2
CDC24 <35492804+CDC24@users.noreply.github.com>
2018-01-31 08:23:55 -0500

    Updated from Brython Server: 1/31/2018 8:23:54 AM

--- coinFlip.py
+++ coinFlip.py
@@ -1,3 +1,14 @@
 #Caleb Callaway
 #1/31/18
 #coinFlip.py - uses random numbers and if statements to display heads or tails
+
+
+
+from random import randint
+
+num = randint(1,2)
+
+if num == 1:
+    print ("sunny side up, that's heads")
+elif num ==2:
+    print ("butt end, that's tails sir")
