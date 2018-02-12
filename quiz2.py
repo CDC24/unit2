0e4979ff3682bed27e5202aeca0deb9f06e933e6
CDC24 <35492804+CDC24@users.noreply.github.com>
2018-02-12 08:23:41 -0500

    Updated from Brython Server: 2/12/2018 8:23:41 AM

--- quiz2.py
+++ quiz2.py
@@ -8,4 +8,20 @@ words = input ("enter two words: ")
 word1, word2 = words.split( )
 
 if len(word1)>len(word2):
-    print (word1,"is longer than",word2)
+    print (word1,"is a longer word than",word2)
+
+elif len(word2)>len(word1):
+    print (word2,"is a longer word than",word1)
+    
+else:
+    print ("These words are the same length.")
+    
+    
+    
+
+    
+if "p" or "P" in word1:
+    print (word1,"has at least 1 p.")
+    
+if "p" or "P" in word2:
+    print (word2,"has at least 1 p.")
